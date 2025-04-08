3c6f661401bf693b3847cdee5d3fb5684c3bbf33
RijalPratama21 <rijal.pratama_ti23@nusaputra.ac.id>
2025-04-08 20:12:27 +0700

    first commit

---
+++ FontAwesome/asset/data.js
@@ -0,0 +1,14 @@
+let usia = 35;
+
+if (usia > 10 && usia < 20) {
+    console.log('anda remaja')
+}
+else if (usia > 18 && usia < 30) {
+    console.log('anda dewasa')
+}
+else if (usia > 30) {
+    console.log("lu mah tua bro sama kaya kakek gua 😁")
+}
+else {
+    console.log('wahh elu usia berapa ya??')
+}
